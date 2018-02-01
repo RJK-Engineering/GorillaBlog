794eab8a4874a85b516b5f8852efd2b0c8a3c3ba
RJK-Engineering <rjklinkhamer@gmail.com>
2018-02-01 16:56:32 +0100

    Fixed CSS url

--- htdocs/index.php
+++ htdocs/index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GorillaBlog</title>
-    <link rel="stylesheet" href="../GorillaBlog.css">
+    <link rel="stylesheet" href="GorillaBlog.css">
 </head>
 <body>
     <h1>GorillaBlog Articles</h1>
