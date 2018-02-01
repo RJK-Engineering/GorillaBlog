51652c5ae825cd9f4e66c12122816fc050f225fa
RJK-Engineering <rjklinkhamer@gmail.com>
2018-02-01 16:03:27 +0100

    Added index.php [W3-002]

---
+++ htdocs/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GorillaBlog</title>
+    <link rel="stylesheet" href="../GorillaBlog.css">
+</head>
+<body>
+    <h1>GorillaBlog Articles</h1>
+
+    <div id="articles"></div>
+
+    <div>Sources available on <a href="https://github.com/RJK-Engineering/GorillaBlog">GitHub</a></div>
+
+    <script src="https://fastcdn.org/MooTools/1.5.2/MooTools-Core-compat-compressed.js"></script>
+    <script src="Articles.js"></script>
+</body>
+</html>
