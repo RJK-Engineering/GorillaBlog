0cd09977cf9ae5159981f17fb2956bee66755cf7
RJK-Engineering <rjklinkhamer@gmail.com>
2018-02-01 20:23:13 +0100

    Added formatText() which translates blog text to html [W3-002]

--- htdocs/Articles.js
+++ htdocs/Articles.js
@@ -22,6 +22,13 @@ var getArticlesRequest = new Request.JSON({
 
 function addArticle(article) {
     var div = $$('#articles');
+    var text = formatText(article.text);
     div.appendHTML('<h3 class="title">' + article.title + '</h3>');
-    div.appendHTML('<div class="article">' + article.text + '</div>');
+    div.appendHTML('<div class="article">' + text + '</div>');
+}
+
+function formatText(text) {
+    text = '<p>' + text + '</p>';
+    text = text.replace(/\n\n/g, '</p><p>');
+    return text;
 }
