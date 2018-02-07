888314af42294b07b9bec3019acd19f957e96a96
RJK-Engineering <rjklinkhamer@gmail.com>
2018-02-07 13:15:24 +0100

    Removed credentials from source code

--- htdocs/GorillaBlogDb.php
+++ htdocs/GorillaBlogDb.php
@@ -1,10 +1,10 @@
 <?php
 
 function DbConnect() {
-    $dbserver = "localhost";
-    $dbname = "gorillablog";
-    $dbuser = "gorillablog";
-    $dbpass = "gorillablog";
+    $dbserver = getenv('GORILLABLOG_DBSERVER');
+    $dbname = getenv('GORILLABLOG_DBNAME');
+    $dbuser = getenv('GORILLABLOG_DBUSER');
+    $dbpass = getenv('GORILLABLOG_DBPASS');
 
     $db = new PDO("mysql:host=$dbserver;dbname=$dbname", $dbuser, $dbpass);
 
